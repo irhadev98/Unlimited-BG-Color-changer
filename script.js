2c6c178461da0c06d853bfaecb80a119eb692f5f
Irha Akram <irhaakram298@gmail.com>
2026-06-29 11:39:28 +0500

    add random colors

---
+++ script.js
@@ -0,0 +1,30 @@
+// Generate a random color
+
+const randomcolor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+    for(let i = 0; i < 6; i++){
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color
+};
+
+let intervalid;
+const startChangingColor = function(){
+    
+    function changebgcolor (){
+        document.body.style.backgroundColor = randomcolor()
+    }
+
+    if (!intervalid){
+        intervalid = setInterval(changebgcolor, 1000)
+    }
+};
+const stopChangingColor = function(){
+    clearInterval(intervalid);
+    intervalid = null;
+};
+
+document.querySelector("#start").addEventListener('click', startChangingColor)
+
+document.querySelector("#stop").addEventListener('click', stopChangingColor)
